ce0985de8c7cc7b1d0c3bbadddf0997260aacde1
Arnaud <59090194+ArnaudS-A5@users.noreply.github.com>
2020-09-22 09:52:39 +0200

    Update HomeController.java
    Remise en place de l'accolade

--- src/main/java/com/reytech/demo/controller/HomeController.java
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -5,8 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController
-{
+public class HomeController {
 
     @GetMapping("/")
     public String hello(){
