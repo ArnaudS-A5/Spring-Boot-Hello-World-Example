7585cd75fe67d11e72e3c42d60e6ecaad97f1d42
Arnaud <59090194+ArnaudS-A5@users.noreply.github.com>
2020-09-22 09:38:05 +0200

    Update HomeController.java
    Accolade à la ligne pour test auto build

--- src/main/java/com/reytech/demo/controller/HomeController.java
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -5,7 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController
+{
 
     @GetMapping("/")
     public String hello(){
